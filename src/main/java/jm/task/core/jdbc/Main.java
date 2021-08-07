325c2db8fb1db4bc8d3058a618eb33d4fdb4512e
Iw <iwor@list.ru>
2021-08-07 22:54:22 +0300

    added Main.java code

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,35 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    private static UserService service;
+    
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+        
+        service = new UserServiceImpl();
+        service.createUsersTable();
+        
+        addUser("Name1", "Surname1", (byte) 11);
+        addUser("Name2", "Surname2", (byte) 22);
+        addUser("Name3", "Surname3", (byte) 33);
+        addUser("Name4", "Surname4", (byte) 44);
+        
+        for (User user : service.getAllUsers()) {
+            System.out.println(user);
+        }
+        
+        service.cleanUsersTable();
+        service.dropUsersTable();
+    }
+    
+    public static void addUser(String name, String lastName, byte age) {
+        service.saveUser(name, lastName, age);
+        System.out.printf("User с именем %s добавлен в базу данных\n", name);
     }
 }
