f049e5224e14ea8206fd88f38227c4683f6b2def
Iw <iwor@list.ru>
2021-08-07 22:32:35 +0300

    added Util.java code

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.*;
+
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    
+    private final String URL = "jdbc:mysql://localhost:3306/db1";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "r00tr00t";
+    
+    private Connection connection;
+    
+    public Util() throws SQLException {
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+    
+    public Connection getConnection() {
+        return connection;
+    }
 }
