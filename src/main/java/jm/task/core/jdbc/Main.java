ee4022cb0faeeecf6e2892b1e0fd658e4a7914a4
Iw <iwor@list.ru>
2021-08-10 23:46:28 +0300

    swiched UserService to Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,15 +1,14 @@
 package jm.task.core.jdbc;
 
+
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.SQLException;
 
 public class Main {
     
-    public static void main(String[] args) throws SQLException {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) {
     
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
