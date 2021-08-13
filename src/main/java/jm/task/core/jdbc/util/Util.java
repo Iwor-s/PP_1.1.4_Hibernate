e10ca35b6ca9320d71f2d8e274c68841c0d9b688
Iw <iwor@list.ru>
2021-08-13 14:45:57 +0300

    edited hibernate properties in Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -18,6 +18,7 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/db1";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "r00tr00t";
+    private static final String DIALECT = "org.hibernate.dialect.MySQL8Dialect";
     
     private static Connection jdbc;
     private static SessionFactory sessionFactory;
@@ -36,24 +37,20 @@ public class Util {
     
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-                Properties properties = new Properties();
-                properties.setProperty(Environment.URL, "jdbc:mysql://localhost:3306/db1");
-                properties.setProperty(Environment.USER, "root");
-                properties.setProperty(Environment.PASS, "r00tr00t");
-                properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-                
-                configuration.setProperties(properties)
-                        .addAnnotatedClass(User.class);
-                
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-                
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            Configuration configuration = new Configuration();
+            Properties properties = new Properties();
+            properties.setProperty(Environment.URL, URL);
+            properties.setProperty(Environment.USER, USERNAME);
+            properties.setProperty(Environment.PASS, PASSWORD);
+            properties.setProperty(Environment.DIALECT, DIALECT);
+            
+            configuration.setProperties(properties)
+                    .addAnnotatedClass(User.class);
+            
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         }
         return sessionFactory;
     }
